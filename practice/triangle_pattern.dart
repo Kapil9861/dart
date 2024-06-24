c419deffbb3e0ace34b51b27e907f893085c354c
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-06-24 23:11:16 +0545

    pattern

---
+++ practice/triangle_pattern.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  triangle();
+}
+
+void triangle() {
+  for (var i = 0; i < 5; i++) {
+    // for (var j = 0; j <= i; j++) {
+    //   stdout.write('*');
+    // }
+    print("*" * i);
+  }
+}
