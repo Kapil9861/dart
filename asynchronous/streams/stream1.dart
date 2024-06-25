4306210285919255a469ca4cdc52691c44af0a5a
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-06-26 00:05:50 +0545

    stream more

--- asynchronous/streams/stream1.dart
+++ asynchronous/streams/stream1.dart
@@ -5,16 +5,23 @@ void main() {
   // Stream.periodic(const Duration(seconds: 2), (x) => -x);
   // Stream.fromFutures([Future(() => 3), Future.value(2)]).listen(print);
 
-  final StreamController streamController = new StreamController<int>();
+  final StreamController streamController =
+      new StreamController<int>.broadcast();
   final streamSubscription = streamController.stream.listen(print);
   var value = 0;
+
+  // normally only a stream is passed to only once and cannot be accessed twice
+  // if needed twice we have to make the stream broadcast from this to line number 8
+  // final StreamController streamController = new StreamController<int>();
+  final otherSubscription = streamController.stream.listen(print);
   Timer.periodic(const Duration(seconds: 1), (timer) {
     if (value == 5) {
       timer.cancel();
       streamController.close();
       streamSubscription.cancel();
+      otherSubscription.cancel();
     } else {
-      print(value++);
+      streamController.add(value++);
     }
   });
 }
